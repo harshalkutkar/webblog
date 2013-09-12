411c3f64f7dcdc4e8d1cce1de331e543c880e1b4
Harsh Alkutkar <harshalkutkar@gmail.com>
2013-09-12 13:15:02 -0400

    Submitted Version

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
 
   def welcome
+       @photos = Photo.all.limit(3);
 
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,6 +15,7 @@ class PhotosController < ApplicationController
   # GET /photos/new
   def new
     @photo = Photo.new
+
   end
 
   # GET /photos/1/edit
@@ -25,6 +26,46 @@ class PhotosController < ApplicationController
   # POST /photos.json
   def create
     @photo = Photo.new(photo_params)
+
+    #------
+    #PART 3: for uploading photos
+
+    # get the file_type that have been uploaded
+    file_type = params[:photo][:picture].content_type
+
+    # check the file type for images
+    if file_type == 'image/jpeg' || file_type == 'image/jpg' || file_type == 'image/png' || file_type == 'image/gif' || file_type == 'image/bmp'
+
+      r = Random.new
+      num = r.rand(1...100)
+
+      # as per the file type give the images name
+      case file_type
+        when "image/jpeg"
+          file_name = "pic_#{Time.now.strftime("%Y%m%d%H%M%S")+num.to_s}.jpg"
+        when "image/png"
+          file_name = "pic_#{Time.now.strftime("%Y%m%d%H%M%S")+num.to_s}.png"
+        when "image/gif"
+          file_name = "pic_#{Time.now.strftime("%Y%m%d%H%M%S")+num.to_s}.gif"
+        when "image/bmp"
+          file_name = "pic_#{Time.now.strftime("%Y%m%d%H%M%S")+num.to_s}.bmp"
+      end
+
+      # give the file path for image
+      file_path = File.join(Rails.root, 'public', 'images', 'upload_images', file_name)
+
+      # copy the image from the uploaded one to file_path
+      File.open(file_path, 'wb') do |f|
+        f.write params[:photo][:picture].read
+      end
+
+
+     @photo.name = file_name
+
+    else
+      puts "Error! Should be in jpeg/png"
+    end
+    #------
 
     respond_to do |format|
       if @photo.save
